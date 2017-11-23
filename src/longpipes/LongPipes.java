53bd1f1f9e70f3efda0addc686d97323de7c126f
up810928 <up810928@S7D40167EA46448.uni.ds.port.ac.uk>
2017-11-23 11:37:54 +0000

    Added ability to round and calculate total order cost for all pipes.

--- src/longpipes/LongPipes.java
+++ src/longpipes/LongPipes.java
@@ -1,6 +1,7 @@
 package longpipes;
 
 import java.awt.Dimension;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import longpipes.pipes.Pipe;
 import longpipes.pipes.Pipe1;
@@ -21,78 +22,89 @@ public class LongPipes {
         ui.setSize(new Dimension(ui.panAdd.getPreferredSize().width + 50, ui.panAdd.getPreferredSize().height + 100));
     }
 
-    public static boolean addPipe(double length, double diameter, int grade, int colour, boolean insulation,
+    public static Pipe addPipe(double length, double diameter, int grade, int colour, boolean insulation,
             boolean reinforcement, boolean resistance, int quantity) {
 
+        Pipe pipe = null;
+        
         //Pipe 1
         if (grade >= 1 && 
             grade <= 3 && 
             colour == 0 && 
             insulation == false && 
             reinforcement == false) {
-            pipes.add(new Pipe1(length, diameter, grade, colour, insulation,
-            reinforcement, resistance, quantity));
-            return true;
+            pipe = new Pipe1(length, diameter, grade, colour, insulation,
+            reinforcement, resistance, quantity);
         }
         //Pipe 2
-        if (grade >= 2 && 
+        else if (grade >= 2 && 
             grade <= 4 && 
             colour == 1 && 
             insulation == false && 
             reinforcement == false) {
-            pipes.add(new Pipe2(length, diameter, grade, colour, insulation,
-            reinforcement, resistance, quantity));
-            return true;
+            pipe = new Pipe2(length, diameter, grade, colour, insulation,
+            reinforcement, resistance, quantity);
         }
         //Pipe 3
-        if (grade >= 2 && 
+        else if (grade >= 2 && 
             grade <= 5 && 
             colour == 2 && 
             insulation == false && 
             reinforcement == false) {
-            pipes.add(new Pipe3(length, diameter, grade, colour, insulation,
-            reinforcement, resistance, quantity));
-            return true;
+            pipe = new Pipe3(length, diameter, grade, colour, insulation,
+            reinforcement, resistance, quantity);
         }
         //Pipe 4
-        if (grade >= 2 && 
+        else if (grade >= 2 && 
             grade <= 5 && 
             colour == 2 && 
             insulation == true && 
             reinforcement == false) {
-            pipes.add(new Pipe4(length, diameter, grade, colour, insulation,
-            reinforcement, resistance, quantity));
-            return true;
+            pipe = new Pipe4(length, diameter, grade, colour, insulation,
+            reinforcement, resistance, quantity);
         }
         //Pipe 5
-        if (grade >= 3 && 
+        else if (grade >= 3 && 
             grade <= 5 && 
             colour == 2 && 
             insulation == true && 
             reinforcement == true) {
-            pipes.add(new Pipe5(length, diameter, grade, colour, insulation,
-            reinforcement, resistance, quantity));
-            return true;
+            pipe = new Pipe5(length, diameter, grade, colour, insulation,
+            reinforcement, resistance, quantity);
         }
-        return false;
-
+        if (pipe != null) pipes.add(pipe);
+        return pipe;
     }
-public static int chkTempType(int grade, String colour){
-        //chkType 0 = type I
-        //chkType 1 = type II
-        //chkType 2 = type III - IV
-        //chkType 3 = type V 
-        int chkType;
-        if (grade <= 3 &&
-                colour == "none")
-            chkType = 0;
-        else if (grade <= 4 &&
-                    colour == "1")
-            chkType = 1;
-        else if (grade == 2 &&
-                    colour == "2")
-            chkType = 2;
-        else chkType = 3;
-        return chkType;
+    public static int chkTempType(int grade, String colour){
+            //chkType 0 = type I
+            //chkType 1 = type II
+            //chkType 2 = type III - IV
+            //chkType 3 = type V 
+            int chkType;
+            if (grade <= 3 &&
+                    colour == "none")
+                chkType = 0;
+            else if (grade <= 4 &&
+                        colour == "1")
+                chkType = 1;
+            else if (grade == 2 &&
+                        colour == "2")
+                chkType = 2;
+            else chkType = 3;
+            return chkType;
+    }
+
+    public static double getTotalOrderCost(){
+        double total = 0;
+        for (Pipe pipe : pipes){
+            total += pipe.getTotalCost();
+        }
+        return total;
+    }
+    
+    public static double round(double input){
+        DecimalFormat df2 = new DecimalFormat(".##");
+        return Double.parseDouble(df2.format(input));
     }
 }
+
